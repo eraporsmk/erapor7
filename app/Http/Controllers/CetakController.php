624ec1a706b7b78f82ea17bba61460469e9c9fca
masadi <masadi.com@gmail.com>
2024-01-22 15:20:25 +0700

    Update: Perbaikan bug di cetak rapor PKL

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -555,6 +555,7 @@ class CetakController extends Controller
 			},
 			'kelas' => function($query){
 				$query->where('rombongan_belajar.semester_id', request()->route('semester_id'));
+				$query->where('tingkat', '<>', 0);
 			},
 			'pd_pkl' => function($query){
 				$query->withWhereHas('praktik_kerja_lapangan', function($query){
